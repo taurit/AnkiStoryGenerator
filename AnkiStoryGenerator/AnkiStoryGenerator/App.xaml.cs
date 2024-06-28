ae05f5dd0e3b944da8fec9ffe42bdee9b29dba0f
Paweł Bulwan <github@taurit.pl>
2024-06-28 11:27:24 +0200

    Add WPF project template

---
+++ AnkiStoryGenerator/AnkiStoryGenerator/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AnkiStoryGenerator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
