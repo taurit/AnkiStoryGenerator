62b83f78056796208d0524de1af8811c0c15fcf7
Paweł Bulwan <github@taurit.pl>
2024-06-29 18:10:10 +0200

    Refactor: use file-scoped namespaces as the default

--- AnkiStoryGenerator/AnkiStoryGenerator.Tests/StringHelpersTests.cs
+++ AnkiStoryGenerator/AnkiStoryGenerator.Tests/StringHelpersTests.cs
@@ -1,57 +1,56 @@
 ﻿using AnkiStoryGenerator.Services;
 
-namespace AnkiStoryGenerator.Tests
+namespace AnkiStoryGenerator.Tests;
+
+[TestClass]
+public class StringHelpersTests
 {
-    [TestClass]
-    public class StringHelpersTests
+    [TestMethod]
+    public void TrimBackticksWrapperFromString_ShouldRemoveBackticksAndContentType_WhenPresent()
     {
-        [TestMethod]
-        public void TrimBackticksWrapperFromString_ShouldRemoveBackticksAndContentType_WhenPresent()
-        {
-            // Arrange
-            string input = @"```html
+        // Arrange
+        string input = @"```html
 This is a code block
 ```";
 
-            // Act
-            string result = StringHelpers.TrimBackticksWrapperFromString(input);
+        // Act
+        string result = StringHelpers.TrimBackticksWrapperFromString(input);
 
-            // Assert
-            Assert.AreEqual("This is a code block", result);
-        }
+        // Assert
+        Assert.AreEqual("This is a code block", result);
+    }
 
-        [TestMethod]
-        public void TrimBackticksWrapperFromString_ShouldRemoveBackticksAndContentType_OnlyAtTheEnd()
-        {
-            // Arrange
-            string input = @"```html
+    [TestMethod]
+    public void TrimBackticksWrapperFromString_ShouldRemoveBackticksAndContentType_OnlyAtTheEnd()
+    {
+        // Arrange
+        string input = @"```html
 This is a code block
 ```
 Some other text
 ```";
 
-            // Act
-            string result = StringHelpers.TrimBackticksWrapperFromString(input);
+        // Act
+        string result = StringHelpers.TrimBackticksWrapperFromString(input);
 
-            // Assert
-            Assert.AreEqual(@"This is a code block
+        // Assert
+        Assert.AreEqual(@"This is a code block
 ```
 Some other text", result);
-        }
-
-
-        [TestMethod]
-        public void TrimBackticksWrapperFromString_ShouldNotRemoveAnything_WhenBackticksAndContentTypeNotPresent()
-        {
-            // Arrange
-            string input = "This is a normal string";
-
-            // Act
-            string result = StringHelpers.TrimBackticksWrapperFromString(input);
-
-            // Assert
-            Assert.AreEqual(input, result);
-        }
-
     }
+
+
+    [TestMethod]
+    public void TrimBackticksWrapperFromString_ShouldNotRemoveAnything_WhenBackticksAndContentTypeNotPresent()
+    {
+        // Arrange
+        string input = "This is a normal string";
+
+        // Act
+        string result = StringHelpers.TrimBackticksWrapperFromString(input);
+
+        // Assert
+        Assert.AreEqual(input, result);
+    }
+
 }
